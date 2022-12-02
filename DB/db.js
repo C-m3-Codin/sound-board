4eb38cb5d97b0a1c9e02bd2c48f402fc24dc31bf
c1337 <cyril199897@gmail.com>
2022-12-02 21:52:18 +0530

    pooling for performance

--- DB/db.js
+++ DB/db.js
@@ -1,67 +1,78 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASS)}@cluster0.unhv19o.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(
+  process.env.DB_PASS
+)}@cluster0.unhv19o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function initDb() {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.log("Couldnt connect to DB: ", error);
+  }
+}
 
 async function getList(guildId) {
-	try {
-		await client.connect();
-		const database = client.db('sources');
-		const collection = database.collection('guild_sources');
-		const doc = await collection.findOne({ _id : guildId });
-		if (!doc) return 'empty';
-		const sources = {};
-		Object.keys(doc).forEach(function(key) {
-			if (key.includes('link') || key.includes('label')) {
-				sources[key] = doc[key];
-			}
-		});
-		return sources;
-	}
-	catch (error) {
-		console.log(error);
-		return 'error';
-	}
-	finally {
-		await client.close();
-	}
+  try {
+    // await client.connect();
+    const database = client.db("sources");
+    const collection = database.collection("guild_sources");
+    const doc = await collection.findOne({ _id: guildId });
+    if (!doc) return "empty";
+    const sources = {};
+    Object.keys(doc).forEach(function (key) {
+      if (key.includes("link") || key.includes("label")) {
+        sources[key] = doc[key];
+      }
+    });
+    return sources;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  } finally {
+    // await client.close();
+  }
 }
 
 async function setLink(guildId, num, label, link) {
-	try {
-		await client.connect();
-		const database = client.db('sources');
-		const collection = database.collection('guild_sources');
-		// create a filter for a movie to update
-		const filter = { _id : guildId };
-		// this option instructs the method to create a document if no documents match the filter
-		const options = { upsert: true };
-		// create a document that sets the plot of the movie
-		const key1 = 'link' + num.toString();
-		const key2 = 'label' + num.toString();
-		const updateDoc = {
-			$set: {
-				[key1]: link,
-				[key2]: label,
-			},
-		};
-		const result = await collection.updateOne(filter, updateDoc, options);
-		console.log(
-			`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
-		);
-		return 'success';
-	}
-	catch (error) {
-		console.log(error);
-		return 'error';
-	}
-	finally {
-		await client.close();
-	}
+  try {
+    // await client.connect();
+    const database = client.db("sources");
+    const collection = database.collection("guild_sources");
+    // create a filter for a movie to update
+    const filter = { _id: guildId };
+    // this option instructs the method to create a document if no documents match the filter
+    const options = { upsert: true };
+    // create a document that sets the plot of the movie
+    const key1 = "link" + num.toString();
+    const key2 = "label" + num.toString();
+    const updateDoc = {
+      $set: {
+        [key1]: link,
+        [key2]: label,
+      },
+    };
+    const result = await collection.updateOne(filter, updateDoc, options);
+    console.log(
+      `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+    );
+    return "success";
+  } catch (error) {
+    console.log(error);
+    return "error";
+  } finally {
+    // await client.close();
+  }
 }
 
 module.exports = {
-	getList,
-	setLink,
+  getList,
+  setLink,
+  initDb,
 };
--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,12 @@
-const { Events } = require('discord.js');
-
+const { Events } = require("discord.js");
+const { initDb } = require("../DB/db");
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+  name: Events.ClientReady,
+  once: true,
+  async execute(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    console.log("Connecting to DB..");
+    await initDb();
+    console.log("Connected to DB");
+  },
 };
